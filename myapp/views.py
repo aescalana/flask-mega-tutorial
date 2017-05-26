bffcdb211f9105df6cd5d8320618b24b31b514be
Alejandro <aescalana@users.noreply.github.com>
2017-05-26 16:33:17 -0500

    Part II - Templates (Step 3 - Loops)

--- myapp/views.py
+++ myapp/views.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/index')
 def index():
 	mockUser = {'nickname':'Manuel'}
-	return render_template('index.html',user=mockUser)
+	mockPosts = [
+		{
+			'author': {'nickname':'John'},
+			'body': 'Beautiful day in Portland'
+		},
+		{
+			'author': {'nickname':'Susan'},
+			'body': 'The Avengers movie was so cool'
+		}
+	]
+	return render_template('index.html',title='Home',user=mockUser, posts=mockPosts)
